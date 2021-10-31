38543b8174716195976edec90d31717dc86e3cad
seiyaKM <seiya.kumada.0107@gmail.com>
2021-11-01 06:22:54 +0900

    6.5 sample sfc

---
+++ chapter06/6.5/hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="message">message: {{ msg }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "hello",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.message {
+  color: aqua;
+}
+</style>
