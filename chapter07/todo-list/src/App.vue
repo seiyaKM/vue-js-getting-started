6329f00a035e0f784fb4630de7b5f37227ccdfa7
seiyaKM <seiya.kumada.0107@gmail.com>
2022-02-17 23:40:13 +0900

    7.5 page 258

---
+++ chapter07/todo-list/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2>タスク一覧</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        <input
+          type="checkbox"
+          :checked="task.done"
+          @change="toggleTaskStatus(task)"
+        />
+        {{ task.name }}
+      </li>
+    </ul>
+    <form @submit.prevent="addTask">
+      <input type="text" v-model="newTaskName" placeholder="new task" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      newTaskName: "",
+    };
+  },
+  computed: {
+    tasks() {
+      return this.$store.state.tasks;
+    },
+  },
+  methods: {
+    addTask() {
+      this.$store.commit("addTask", {
+        name: this.newTaskName,
+      });
+      this.newTaskName = "";
+    },
+    toggleTaskStatus(task) {
+      this.$store.commit("toggleTaskStatus", {
+        id: task.id,
+      });
+    },
+  },
+};
+</script>
