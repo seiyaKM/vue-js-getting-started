cb337bd541ca8ce495bb09ee5d81c2c7bbfa1a96
seiyaKM <seiya.kumada.0107@gmail.com>
2022-02-20 06:46:31 +0900

    7.5 page 262

--- chapter07/todo-list/src/App.vue
+++ chapter07/todo-list/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>タスク一覧</h2>
+    <h2>Task List</h2>
     <ul>
       <li v-for="task in tasks" :key="task.id">
         <input
@@ -8,11 +8,26 @@
           :checked="task.done"
           @change="toggleTaskStatus(task)"
         />
-        {{ task.name }}
+        {{ task.name }} -
+        <span v-for="id in task.labelIds" :key="id">
+          {{ getLabelText(id) }}
+        </span>
       </li>
     </ul>
     <form @submit.prevent="addTask">
       <input type="text" v-model="newTaskName" placeholder="new task" />
+    </form>
+
+    <h2>Label List</h2>
+    <ul>
+      <li v-for="label in labels" :key="label.id">
+        <input type="checkbox" :value="label.id" v-model="newTaskLabelIds" />
+        {{ label.text }}
+      </li>
+    </ul>
+
+    <form @submit.prevent="addLabel">
+      <input type="text" v-model="newLabelText" placeholder="new label" />
     </form>
   </div>
 </template>
@@ -23,24 +38,42 @@ export default {
   data() {
     return {
       newTaskName: "",
+      newTaskLabelIds: [],
+      newLabelText: "",
     };
   },
   computed: {
     tasks() {
       return this.$store.state.tasks;
     },
+    labels() {
+      return this.$store.state.labels;
+    },
   },
   methods: {
     addTask() {
       this.$store.commit("addTask", {
         name: this.newTaskName,
+        labelIds: this.newTaskLabelIds,
       });
       this.newTaskName = "";
+      this.newTaskLabelIds = [];
     },
     toggleTaskStatus(task) {
       this.$store.commit("toggleTaskStatus", {
         id: task.id,
       });
+    },
+
+    addLabel() {
+      this.$store.commit("addLabel", {
+        text: this.newLabelText,
+      });
+      this.newLabelText = "";
+    },
+    getLabelText(id) {
+      const label = this.labels.find((label) => label.id === id);
+      return label ? label.text : "";
     },
   },
 };
--- chapter07/todo-list/src/store.js
+++ chapter07/todo-list/src/store.js
@@ -9,22 +9,41 @@ const store = new Vuex.Store({
       {
         id: 1,
         name: "牛乳を買う",
+        labelIds: [1, 2],
         done: false,
       },
       {
         id: 2,
         name: "Vue.jsの本を買う",
+        labelIds: [1, 3],
         done: true,
       },
     ],
     nextTaskId: 3,
+
+    labels: [
+      {
+        id: 1,
+        text: "shopping",
+      },
+      {
+        id: 2,
+        text: "foof",
+      },
+      {
+        id: 3,
+        text: "book",
+      },
+    ],
+    nextLabelId: 4,
   },
 
   mutations: {
-    addTask(state, { name }) {
+    addTask(state, { name, labelIds }) {
       state.tasks.push({
         id: state.nextTaskId,
         name,
+        labelIds,
         done: false,
       });
       state.nextTaskId++;
@@ -37,6 +56,14 @@ const store = new Vuex.Store({
       filtered.forEach((task) => {
         task.done = !task.done;
       });
+    },
+
+    addLabel(state, { text }) {
+      state.labels.push({
+        id: state.nextLabelId,
+        text,
+      });
+      state.nextLabelId++;
     },
   },
 });
